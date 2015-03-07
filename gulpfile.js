72aa5a50f08c247d0ccd4103292c8a595e685918
darthmaim <darth.maim@arcor.de>
2015-03-08 00:01:26 +0100

    refactored gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,88 +2,104 @@
 
 var gulp = require('gulp');
 
-var paths = {
-    images: {
-        src: 'resources/assets/images/*',
-        dest: 'public/assets/images'
+// === IMAGES ===
+
+var images = {
+    src: 'resources/assets/images/*',
+    dest: 'public/assets/images',
+
+    build: function() {
+        var files = gulp.src( images.src );
+        return images.handle( files );
     },
-    css: {
-        src: 'resources/assets/css/*',
-        dest: 'public/assets/css'
+    watch: function() {
+        var watch = require( 'gulp-watch' );
+        var plumber = require('gulp-plumber');
+
+        var files = watch( images.src ).pipe( plumber() );
+        return images.handle( files );
+    },
+    handle: function( source ) {
+        var imagemin = require('gulp-imagemin');
+        var changed = require('gulp-changed');
+
+        var imageminConfig = {
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }]
+        };
+
+        return source
+            .pipe( changed( images.dest ))
+            .pipe( imagemin( imageminConfig ))
+            .pipe( gulp.dest( images.dest ));
     }
 };
 
-// === IMAGES ===
+// === STYLES ===
 
-var images = function( source ) {
-    var imagemin = require('gulp-imagemin')({
-        progressive: true,
-        svgoPlugins: [{ removeViewBox: false }]
-    });
-    var changed = require('gulp-changed');
+var styles = {
+    src: 'resources/assets/css/*',
+    dest: 'public/assets/css',
 
-    return source
-        .pipe( changed( paths.images.dest ))
-        .pipe( imagemin )
-        .pipe( gulp.dest( paths.images.dest ));
+    build: function() {
+        styles.buildCss();
+        styles.normalizeCss();
+    },
+    buildCss: function() {
+        var sass = require('gulp-sass');
+        var autoprefixer = require('gulp-autoprefixer');
+        var minify = require('gulp-minify-css');
+        var sourcemaps = require('gulp-sourcemaps');
+
+        return gulp.src( styles.src )
+            .pipe( sourcemaps.init() )
+            .pipe( sass() )
+            .pipe( autoprefixer() )
+            .pipe( minify() )
+            .pipe( sourcemaps.write( '.' ))
+            .pipe( gulp.dest( styles.dest ))
+    },
+    normalizeCss: function () {
+        var minify = require('gulp-minify-css');
+        var changed = require('gulp-changed');
+        return gulp.src('node_modules/normalize.css/normalize.css')
+            .pipe( changed( styles.dest ))
+            .pipe( minify() )
+            .pipe( gulp.dest( styles.dest ));
+    },
+    watch: function() {
+        var watch = require('gulp-watch');
+        return watch( styles.src, styles.buildCss );
+    }
 };
-
-gulp.task('images', function() {
-    return images( gulp.src( paths.images.src ));
-});
-
-// === CSS ===
-
-gulp.task('css', function() {
-    var sass = require('gulp-sass');
-    var autoprefixer = require('gulp-autoprefixer');
-    var minify = require('gulp-minify-css');
-    var sourcemaps = require('gulp-sourcemaps');
-
-    return gulp.src( paths.css.src )
-        .pipe( sourcemaps.init() )
-        .pipe( sass() )
-        .pipe( autoprefixer() )
-        .pipe( minify() )
-        .pipe( sourcemaps.write( '.' ))
-        .pipe( gulp.dest( paths.css.dest ))
-});
-
-gulp.task('copy:normalize', function () {
-    var minify = require('gulp-minify-css');
-    var changed = require('gulp-changed');
-    return gulp.src( 'node_modules/normalize.css/normalize.css' )
-        .pipe( changed( paths.css.dest ))
-        .pipe( minify() )
-        .pipe( gulp.dest( paths.css.dest ));
-});
 
 // === GENERAL ===
 
-gulp.task('watch', function() {
-    var watch = require('gulp-watch');
-    var plumber = require('gulp-plumber');
+var general = {
+    src: 'resources/assets',
+    dest: 'public/assets',
 
-    gulp.start( 'css' );
-    watch( paths.css.src, function() {
-        gulp.start( 'css' );
-    });
+    build: function() {
+        images.build();
+        styles.build();
+    },
+    clean: function( cb ) {
+        var del = require('del');
+        del( general.dest, cb );
+    },
+    watch: function() {
+        general.build();
 
-    images( gulp.src( paths.images.src )
-        .pipe( watch( paths.images.src ))
-        .pipe( plumber() )
-    );
-});
+        styles.watch();
+        images.watch();
+    }
+};
 
-gulp.task('clean', function( callback ) {
-    var del = require('del');
-    del([
-        'public/assets'
-    ], callback);
-});
-gulp.task('build', ['images', 'copy:normalize']);
-gulp.task('build-clean', ['clean'], function() {
-    gulp.start( 'build' );
-});
-
-gulp.task('default', ['build']);
+gulp.task( 'watch', general.watch );
+gulp.task( 'clean', general.clean );
+gulp.task( 'build', general.build );
+gulp.task( 'build:css', styles.build );
+gulp.task( 'build:images', images.build );
+gulp.task( 'build-clean', ['clean'], general.build );
+// alias default = build
+gulp.task( 'default', ['build'] );
