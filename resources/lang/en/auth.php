fea4e11c4ed64383d9520f4dc522750bb43e5c6b
smiley <smiley@chillerlan.net>
2015-03-07 20:26:35 +0100

    auth translations

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+	'login-header' => 'Login',
+	'login-button' => 'Login',
+	'login-hint' => 'Do you want to log-in?',
+	'password-reset-header' => 'Password reset',
+	'password-reset-mail-button' => 'Send E-Mail',
+	'password-reset-button' => 'Reset password',
+	'password-reset-hint' => 'Did you forget your password?',
+	'register-header' => 'Register',
+	'register-button' => 'Register',
+	'register-hint' => 'New to GW2Hero.es? Sign up!',
+	'name' => 'Name',
+	'name-placeholder' => 'Your Name',
+	'email' => 'E-Mail',
+	'email-placeholder' => 'your.email@example.com',
+	'password' => 'Password',
+	'password-confirm' => 'Password confirm',
+	'remember-login' => 'Want to stay logged in?',
+	'input-errors' => 'Whoops! There was something wrong with your input.',
+#	'' => '',
+];
