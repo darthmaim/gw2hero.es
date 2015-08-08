21b8a6792c598834030d28a7d51db3a4d0c44f3a
darthmaim <darth.maim@arcor.de>
2015-08-08 10:17:13 +0200

    move character name route redirection into own method in CharacterController

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -1,31 +1,37 @@
 <?php namespace GW2Heroes\Http\Controllers;
 
 use GW2Heroes\Character;
+use Illuminate\Http\Exception\HttpResponseException;
 use Illuminate\Http\Request;
 
-class CharacterController extends Controller{
-    public function getIndex( Request $request, $id ) {
+class CharacterController extends Controller {
+
+    protected function getCharacterFromRequest( Request $request, $id, $function ) {
         $id = base_convert( $id, 36, 10 );
 
         /** @var Character $character */
         $character = Character::with('account', 'account.user')->find($id);
 
-        if( $character->getUrl() !== $request->url() ) {
-            return redirect( $character->getUrl(), 301 );
+        $actionName = '\\'.__CLASS__.'@'.$function;
+        $action = action( $actionName, $character->getActionData() );
+
+        if( $action !== $request->url() ) {
+            throw new HttpResponseException(
+                redirect( $action, 301 )
+            );
         }
+
+        return $character;
+    }
+
+    public function getIndex( Request $request, $id ) {
+        $character = $this->getCharacterFromRequest( $request, $id, __FUNCTION__ );
 
         return view('character.summary', compact('character'));
     }
 
     public function getActivities( Request $request, $id ) {
-        $id = base_convert( $id, 36, 10 );
-
-        /** @var Character $character */
-        $character = Character::with('account', 'account.user')->find($id);
-
-        if( action( 'CharacterController@getActivities', $character->getActionData() ) !== $request->url() ) {
-            return redirect()->action('CharacterController@getActivities', $character->getActionData(), 301);
-        }
+        $character = $this->getCharacterFromRequest( $request, $id, __FUNCTION__ );
 
         $activities = $character->activities()
             ->with('character', 'account', 'user', 'user.accounts')
