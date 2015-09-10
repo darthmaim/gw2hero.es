4639ab3513cfcdd1d5f69cf88c37f191ba682126
darthmaim <darth.maim@arcor.de>
2015-09-10 14:37:24 +0200

    removed missed entrust trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract{
 
-	use Authenticatable, CanResetPassword, EntrustUserTrait;
+	use Authenticatable, CanResetPassword;
 
 	/**
 	 * The database table used by the model.
