c854fab5243b26bddf39bac166918d7a8addae0e
darthmaim <darth.maim@arcor.de>
2015-09-15 20:23:32 +0200

    format equipment

--- resources/views/helper/itemSlot.blade.php
+++ resources/views/helper/itemSlot.blade.php
@@ -2,14 +2,26 @@
     @if(!is_null($item))
         @if(!is_null($item->item))
             <a href="https://gw2treasures.com/item/{{ $item->item_id }}">
-                {{ $item->item->getIcon(32) }} {{ $item->item->name_en }}</a>
+                {{ $item->item->getIcon(32) }}{{ $item->item->name_en }}</a>
         @else
-            <a href="https://gw2treasures.com/item/{{ $item->item_id }}">{{ $item->item_id }}</a>
+            <a class="itemSlot__empty"
+               href="https://gw2treasures.com/item/{{ $item->item_id }}">[{{ $item->item_id }}]</a>
         @endif
         @if(isset($item->data->upgrades))
-            upgraded with {{ implode(', ', $item->data->upgrades) }}
+            @foreach($item->data->upgrades as $upgrade_id)
+                <?php $upgrade = \GW2Heroes\Models\Item::find($upgrade_id); ?>
+                <div class="itemSlot__upgrade">
+                    @if( !is_null( $upgrade ))
+                        <a href="https://gw2treasures.com/item/{{ $upgrade_id }}">
+                            {{ $upgrade->getIcon(24) }}{{ $upgrade->name_en }}</a>
+                    @else
+                        <a class="itemSlot__empty"
+                           href="https://gw2treasures.com/item/{{ $upgrade_id }}">[{{ $upgrade_id}}]</a>
+                    @endif
+                </div>
+            @endforeach
         @endif
     @else
-        <i>Empty</i>
+        <i class="itemSlot__empty">Empty</i>
     @endif
 </div>
