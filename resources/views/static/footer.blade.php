5b172dff2a951b8e25667e9e21a4f539e2e99610
darthmaim <darth.maim@arcor.de>
2015-09-15 22:42:13 +0200

    add some links to footer

--- resources/views/static/footer.blade.php
+++ resources/views/static/footer.blade.php
@@ -1,5 +1,17 @@
 <footer class="footer" id="footer" role="contentinfo">
     @section('footer')
-        <a href="{{ url('/') }}">gw2hero.es</a> &copy; 2015
+        <a href="{{ url('/') }}">gw2hero.es</a> &copy; {{ date('Y') }}
+        <div style="margin-top: 8px; line-height: 1.25;">
+            <a href="https://trello.com/b/lUJa4FuH/gw2heroes"
+               title="Post suggestions on our Trello board">Trello</a> &bull;
+            <a href="https://github.com/chillerlan/gw2hero.es"
+               title="View and contribute to our source code on github">github</a><br>
+            <a href="https://twitter.com/gw2heroes"
+               title="Follow us on twitter">@gw2heroes</a> &bull;
+            <a href="https://www.facebook.com/gw2heroes"
+               title="Like us on facebook">facebook</a> &bull;
+            <a href="mailto:info@gw2hero.es"
+               title="Contact us info@gw2hero.es">info@gw2hero.es</a>
+        </div>
     @show
 </footer>
