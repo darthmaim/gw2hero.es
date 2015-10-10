23bf37c7ccc62e50cf706426d5bf74ad0b336bba
darthmaim <darth.maim@arcor.de>
2015-10-10 22:57:52 +0200

    fix optimization trying to compile non existing files

--- config/compile.php
+++ config/compile.php
@@ -16,10 +16,10 @@ return [
 	'files' => [
 
 		realpath(__DIR__.'/../app/Providers/AppServiceProvider.php'),
-		realpath(__DIR__.'/../app/Providers/BusServiceProvider.php'),
-		realpath(__DIR__.'/../app/Providers/ConfigServiceProvider.php'),
+		realpath(__DIR__.'/../app/Providers/AuthServiceProvider.php'),
 		realpath(__DIR__.'/../app/Providers/EventServiceProvider.php'),
 		realpath(__DIR__.'/../app/Providers/RouteServiceProvider.php'),
+		realpath(__DIR__.'/../app/Providers/ViewServiceProvider.php'),
 
 	],
 
